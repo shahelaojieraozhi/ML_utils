d3e6b05579e0854fb5dace2d9a2a769ee3278940
“shahelaojieraozhi” <1445692095@qq.com>
2023-05-17 16:43:20 +0800

    add GG

--- ML_utils/GG.py
+++ ML_utils/GG.py
@@ -5,12 +5,11 @@ python3
 @Author : RAO ZHI
 @Email : raozhi@mails.cust.edu.cn
 -------------------------------
-@File : niubi.py
+@File : GG.py
 @Software : PyCharm
-@Time : 2023/5/17 16:40
+@Time : 2023/5/17 16:42
 -------------------------------
 """
 
-print('1')
-
-print('test')
+for i in range(6):
+    print('x')
